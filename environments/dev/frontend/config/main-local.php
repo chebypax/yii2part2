a7efdf8f6e89b22cf30d9687f7f650c3d3e71f26
chebypax <chebypax@mail.ru>
2019-05-16 15:34:12 +0300

    Открыта дебаг панель

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -16,6 +16,7 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*']
     ];
 
     $config['bootstrap'][] = 'gii';
